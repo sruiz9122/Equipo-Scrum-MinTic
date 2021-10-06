5caec9ae5fbdef47c8a4eb23eb87c802756aba9f
Sergio Ruiz <sruizb@homecenter.co>
2021-10-06 09:34:38 -0500

    ADD: Configuracion cadena de conexión firebase

--- js/registroProducto.js
+++ js/registroProducto.js
@@ -1,9 +1,47 @@
+import { app } from "./configDB";
 
+let strDescPrd = document.getElementById("inputDescripcionProducto")
+let numValor = document.getElementById("inputValorUnitarioProducto")
+let strEstado = document.getElementById("inputEstadoProducto")
 
 let boton = document.getElementById('botonRegistrarProducto');
-        //boton.click();
-       // boton.click()==undefined ?console.log("es undefined") : console.log("Falso")
-        /*if(boton.onclick){
-            alert("click con datos");
-        }*/
-        boton.onclick = () => {console.log("click");}
+
+boton.addEventListener("click", datosEntrada)
+
+//--Función de escritura
+function writeProductData(descProd, valorUni, estado) {
+    const db = getFirestore(app);
+    set(ref(db, 'productos/'), {
+        descripcion: descProd,
+        valor: `$ ${valorUni}`,
+        estado: estado
+    });
+}
+
+function datosEntrada() {
+    console.clear()
+
+    let descProd = strDescPrd.value
+    let valorUni = numValor.value
+    let estado = strEstado.value
+
+    if (estado == 1) {
+        estado = `Disponible`
+    } else if (estado == 2) {
+        estado = `No Disponible`
+    }
+
+
+    console.log(descProd, `$ ${valorUni}`, estado)
+
+    writeProductData(descProd, valorUni, estado)
+}
+
+//boton.click();
+// boton.click()==undefined ?console.log("es undefined") : console.log("Falso")
+/*if(boton.onclick){
+    alert("click con datos");
+}*/
+//boton.onclick = () => { console.log("click", datosEntrada); }
+
+
