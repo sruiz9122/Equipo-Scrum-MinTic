4d65d0191b8eeac6cf4b6890f8fd1f168dec2406
Andres-B-Developer <faburbano@unicauca.edu.co>
2021-10-09 17:46:29 -0500

    MOD: Buscar, actualizar, desde frontend

--- js/listarProductosDB.js
+++ js/listarProductosDB.js
@@ -19,6 +19,7 @@ let strDescripcionProducto = document.getElementById("inputDescripcionProducto")
 let numVal = document.getElementById("valor_unitario");
 let strEstado = document.getElementById("estadoproducto");
 let btnbuscar = document.getElementById("botonBuscarProducto");
+// let btnEditarProducto = document.
 let btnlimpiar = document.getElementById("botonLimpiarBusqueda");
 let strEstadoConfirm = document.getElementById("edicionEstado");
 let btnConfirmar = document.getElementById("botonConfirmar");
@@ -74,9 +75,17 @@ function pintarBrowser(elementos) {
     console.log(elementos);
     let contenido = ``;
     elementos.forEach(element => {
-        contenido += `<tr><th scope="row">${element.id}</th><th>${element.descripcion}</th><th>${element.valor}</th><th>${element.estado}</th><th><input type="button" value="Editar" id="teditar" class="btn btn-dark" /></th></tr>`;
+        contenido += `<tr>
+                        <td scope="row">${element.id}</td>
+                        <td>${element.descripcion}</td>
+                        <td>${element.valor}</td>
+                        <td>${element.estado}</td>
+                        <td><input type="button" value="Editar" id="${element.id}" class="botonEditar btn btn-dark" /></td>
+                    </tr>`;
         $('#bodyTabla').html(contenido);
     });
+
+    cargarBotones();
     /* contendorTarea.innerHTML = contenidoHtml; */
 }
 
@@ -101,6 +110,7 @@ async function leerTareas() {
     return tareas;
 }
 
+//DB Propios
 const leerProductos = async () => {
     const productos = [];
     const respuesta = await database.collection('productos').get();
@@ -118,28 +128,124 @@ btnbuscar.addEventListener('click', (e) => {
 });
 
 // Login
-btnLogin.addEventListener('click', (e) => {
-    login();
-    // console.dir(btnLogin);
-    btnLogin.classList.add('hidden');
-    btnLogOut.classList.remove('hidden');
-    formulario.classList.remove('hidden');
+// btnLogin.addEventListener('click', (e) => {
+//     login();
+//     // console.dir(btnLogin);
+//     btnLogin.classList.add('hidden');
+//     btnLogOut.classList.remove('hidden');
+//     formulario.classList.remove('hidden');
 
-});
+// });
 // Logout
-btnLogOut.addEventListener('click', (e) => {
-    logOut();
-    btnLogin.classList.remove('hidden');
-    btnLogOut.classList.add('hidden');
-    formulario.classList.add('hidden');
-});
+// btnLogOut.addEventListener('click', (e) => {
+//     logOut();
+//     btnLogin.classList.remove('hidden');
+//     btnLogOut.classList.add('hidden');
+//     formulario.classList.add('hidden');
+// });
 
 // formulario
-formulario.addEventListener('submit', (e) => {
+// formulario.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     console.log(input.value);
+//     const texto = input.value;
+//     if (texto !== "") {
+//         adicionarTarea(texto);
+//     }
+// });
+
+
+let idRegistro = 0;
+
+const cargarBotones = () => {
+
+    $(".botonEditar").click(function (e) {
+        //Obtenemos hijos del padre <div> desde el target
+        //console.log("hiciste click");
+        // console.log(e);
+        // console.log(e.target.id);
+        idRegistro = e.target.id;
+        // console.log($(e.target).parents().eq(1)[0]);
+        // console.log($(e.target).parents().eq(1).children());
+        // console.log($(e.target).parents().eq(1).children()[1].innerHTML);
+        // console.log($(e.target).parents().eq(1).children()[3].innerHTML);
+
+        // console.log($("#estadoproducto"));
+        // console.log($("#estadoproducto").eq(0));
+        // console.log($("#estadoproducto").eq(0)[0]);
+
+        // console.log($("#estadoproducto optgroup"));
+        // console.log($("#estadoproducto optgroup").children());
+        // console.log($("#estadoproducto optgroup").children()[1].innerHTML);
+
+
+        const descripcionProducto = $(e.target).parents().eq(1).children()[1].innerHTML;
+        const valorProducto = $(e.target).parents().eq(1).children()[2].innerHTML;
+        const estadoProducto = $(e.target).parents().eq(1).children()[3].innerHTML;
+
+
+        // console.log($("#descripcion_producto"))
+
+        $("#descripcion_producto").val(descripcionProducto);
+        $("#valor_unitario").val(valorProducto);
+        $("#inputEstadoProducto").html(``);
+        estadoProducto == "Disponible" ?
+            $("#inputEstadoProducto").append(`      
+        
+           <option value="">Seleccione</option>
+           <option value="1" selected>Disponible</option>
+           <option value="2">No Disponible</option>
+        
+      `) :
+            $("#inputEstadoProducto").append(`      
+      
+         <option value="">Seleccione</option>
+         <option value="1" >Disponible</option>
+         <option value="2" selected>No Disponible</option>
+      
+    `);
+
+    });
+};
+
+$('#botonConfirmar').click((e) => {
     e.preventDefault();
-    console.log(input.value);
-    const texto = input.value;
-    if (texto !== "") {
-        adicionarTarea(texto);
+
+    const descripcionProducto = $("#descripcion_producto").val();
+    const valorProducto = $("#valor_unitario").val();
+    // const estadoProducto = $("#estadoproducto optgroup").val();
+    const estadoProducto = $("#inputEstadoProducto").val();
+
+    const textoEstadoProducto = $("#inputEstadoProducto option:selected").text();
+    //  console.log(textoEstadoProducto)
+    if (!descripcionProducto || !valorProducto || !estadoProducto) {
+        alert("No deben exsitir campos vacios");
+        console.log(descripcionProducto);
+        console.log(valorProducto);
+        console.log(estadoProducto);
+        console.log(textoEstadoProducto);
+    } else {
+        console.log("Campos completos");
+        console.log(descripcionProducto);
+        console.log(valorProducto);
+        console.log(estadoProducto);
+        console.log(textoEstadoProducto);
     }
+
+
+
+        database.collection("productos").where("id", "==", idRegistro)
+        .get()
+        .then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                console.log(doc.id, " => ", doc.data());
+                // Build doc ref from doc.id
+                database.collection("productos").doc(doc.id).update({
+                    descripcion: descripcionProducto,
+                    estado: textoEstadoProducto,
+                    valor: valorProducto
+                });
+            });
+        }).then(alert("Actulizado con éxito")).then(()=> buscarDB());
+
 });
