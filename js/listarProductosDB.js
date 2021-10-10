f34d6005c5e0d1f2e3b9d1742644e5945ddb8593
Andres-B-Developer <faburbano@unicauca.edu.co>
2021-10-09 19:42:48 -0500

    MOD listarProductosDB: Codigo comentado y limpio

--- js/listarProductosDB.js
+++ js/listarProductosDB.js
@@ -1,18 +1,11 @@
 //   Declarar variables globales
-// const auth = firebase.auth()
-// const proveedor = new firebase.auth.GoogleAuthProvider()
 const database = firebase.firestore();
-// let usuarioActual;
 let listaProductos = [];
+// Variable para guardar el ID del Producto a Editar
+let idRegistro = 0;
+
 
 // Variables DOM
-//ejemplo profe
-const btnLogin = document.getElementById('button-login');
-const btnLogOut = document.getElementById('button-logout');
-const formulario = document.getElementById('todo-form');
-const input = document.getElementById('todo-input');
-const contendorTarea = document.getElementById('todos-container');
-
 //del proyecto
 let numId = document.getElementById("inputIdProducto");
 let strDescripcionProducto = document.getElementById("inputDescripcionProducto");
@@ -25,22 +18,8 @@ let strEstadoConfirm = document.getElementById("edicionEstado");
 let btnConfirmar = document.getElementById("botonConfirmar");
 const NODATA = "No se encuentran datos";
 
-async function login() {
-    try {
-        const respuesta = await auth.signInWithPopup(proveedor);
-        console.log(respuesta.user.displayName);
-        usuarioActual = respuesta.user;
 
-        listaTareas = await leerTareas();
-
-        pintarBrowser(listaTareas);
-
-    } catch (error) {
-        console.error(error);
-        throw new Error(error);
-    }
-}
-
+//Función que busca y e implmeenta pintar en pantalla la tabla con los resultados de búsqueda
 const buscarDB = async () => {
     try {
         listaProductos = await leerProductos();
@@ -51,27 +30,8 @@ const buscarDB = async () => {
     }
 };
 
-function logOut() {
-    auth.signOut();
-}
-
-async function adicionarTarea(texto) {
-    const tarea = {
-        id: uuid.v4(),
-        tarea: texto,
-        completada: false,
-        user: usuarioActual.displayName
-    };
-    const respuesta = await guardarTarea(tarea);
-    console.log(respuesta);
-    input.value = '';
-    // console.log(database.collection('lista-tareas').add(tarea))
-    listaTareas = await leerTareas();
-    pintarBrowser(listaTareas);
-}
-
-function pintarBrowser(elementos) {
-    // contendorTarea
+//Función para pintar la tabla de resultados de búsqueda
+function pintarBrowser(elementos) {    
     console.log(elementos);
     let contenido = ``;
     elementos.forEach(element => {
@@ -89,28 +49,8 @@ function pintarBrowser(elementos) {
     /* contendorTarea.innerHTML = contenidoHtml; */
 }
 
-// Base de datos
-async function guardarTarea(task) {
-    try {
-        const respuesta = await database.collection('lista-tareas').add(task);
-        return respuesta;
-    } catch (error) {
-        console.error(error);
-        throw new Error(error);
-    }
-}
-
-async function leerTareas() {
-    const tareas = [];
-    const respuesta = await database.collection('lista-tareas').get();
-    respuesta.forEach(function (item) {
-        // console.log(item.data());
-        tareas.push(item.data());
-    });
-    return tareas;
-}
-
-//DB Propios
+//Base de datos--------------------------------
+//Función que hace un GetAll de todos los registros
 const leerProductos = async () => {
     const productos = [];
     const respuesta = await database.collection('productos').get();
@@ -121,70 +61,31 @@ const leerProductos = async () => {
     return productos;
 };
 
-// Eventos
-//Buscar(listar)
+// Eventos---------------------------------------
+//Evento Buscar(listar)
 btnbuscar.addEventListener('click', (e) => {
     buscarDB();
 });
 
-// Login
-// btnLogin.addEventListener('click', (e) => {
-//     login();
-//     // console.dir(btnLogin);
-//     btnLogin.classList.add('hidden');
-//     btnLogOut.classList.remove('hidden');
-//     formulario.classList.remove('hidden');
-
-// });
-// Logout
-// btnLogOut.addEventListener('click', (e) => {
-//     logOut();
-//     btnLogin.classList.remove('hidden');
-//     btnLogOut.classList.add('hidden');
-//     formulario.classList.add('hidden');
-// });
-
-// formulario
-// formulario.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     console.log(input.value);
-//     const texto = input.value;
-//     if (texto !== "") {
-//         adicionarTarea(texto);
-//     }
-// });
-
-
-let idRegistro = 0;
-
+//Función para cargar información al formularoi de edición
 const cargarBotones = () => {
-
     $(".botonEditar").click(function (e) {
-        //Obtenemos hijos del padre <div> desde el target
-        //console.log("hiciste click");
-        // console.log(e);
-        // console.log(e.target.id);
         idRegistro = e.target.id;
+
+        // LOGS de Testing-----------------------------------:
+        // console.log(e);
+        // console.log(e.target.id);        
         // console.log($(e.target).parents().eq(1)[0]);
         // console.log($(e.target).parents().eq(1).children());
-        // console.log($(e.target).parents().eq(1).children()[1].innerHTML);
         // console.log($(e.target).parents().eq(1).children()[3].innerHTML);
-
-        // console.log($("#estadoproducto"));
-        // console.log($("#estadoproducto").eq(0));
         // console.log($("#estadoproducto").eq(0)[0]);
-
         // console.log($("#estadoproducto optgroup"));
         // console.log($("#estadoproducto optgroup").children());
         // console.log($("#estadoproducto optgroup").children()[1].innerHTML);
 
-
         const descripcionProducto = $(e.target).parents().eq(1).children()[1].innerHTML;
         const valorProducto = $(e.target).parents().eq(1).children()[2].innerHTML;
         const estadoProducto = $(e.target).parents().eq(1).children()[3].innerHTML;
-
-
-        // console.log($("#descripcion_producto"))
 
         $("#descripcion_producto").val(descripcionProducto);
         $("#valor_unitario").val(valorProducto);
@@ -205,9 +106,9 @@ const cargarBotones = () => {
     });
 };
 
+//Evento asociado al boton de Confirmar Edición
 $('#botonConfirmar').click((e) => {
     e.preventDefault();
-
     const descripcionProducto = $("#descripcion_producto").val();
     const valorProducto = $("#valor_unitario").val();
     // const estadoProducto = $("#estadoproducto optgroup").val();
@@ -217,18 +118,18 @@ $('#botonConfirmar').click((e) => {
     //  console.log(textoEstadoProducto)
     if (!descripcionProducto || !valorProducto || !estadoProducto) {
         alert("No deben exsitir campos vacios");
-        console.log(descripcionProducto);
-        console.log(valorProducto);
-        console.log(estadoProducto);
-        console.log(textoEstadoProducto);
+        // TESTING------------------------:
+        // console.log(descripcionProducto);
+        // console.log(valorProducto);
+        // console.log(estadoProducto);
+        // console.log(textoEstadoProducto);
     } else {
-        console.log("Campos completos");
-        console.log(descripcionProducto);
-        console.log(valorProducto);
-        console.log(estadoProducto);
-        console.log(textoEstadoProducto);
+        // console.log("Campos completos");
+        // console.log(descripcionProducto);
+        // console.log(valorProducto);
+        // console.log(estadoProducto);
+        // console.log(textoEstadoProducto);
     }
-
 
 
     database.collection("productos").where("id", "==", idRegistro)
@@ -236,7 +137,6 @@ $('#botonConfirmar').click((e) => {
         .then(function (querySnapshot) {
             querySnapshot.forEach(function (doc) {
                 console.log(doc.id, " => ", doc.data());
-                // Build doc ref from doc.id
                 database.collection("productos").doc(doc.id).update({
                     descripcion: descripcionProducto,
                     estado: textoEstadoProducto,
@@ -247,6 +147,7 @@ $('#botonConfirmar').click((e) => {
 
 });
 
+//Función que limpia los campos del formulario de edición una vez se confirma la actualización de los datos
 const resetCamposFormulario = () => {
     $("#descripcion_producto").val("");
     $("#valor_unitario").val("");
