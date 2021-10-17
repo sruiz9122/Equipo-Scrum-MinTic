00acebbf887ab7a3b3668d728def40723884b422
Andres-B-Developer <faburbano@unicauca.edu.co>
2021-10-16 22:56:43 -0500

    ADD: Producto y Productos: CRUD de Productos

---
+++ venta-gasolina/src/components/Producto.jsx
@@ -0,0 +1,207 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import {
+  actualizarDocumentoDatabase,
+  consultarDocumentoDatabase,
+  buscarDocumentoFiltrado,
+  guardarDatabase,
+  actualizarDocumentoFiltrado,
+} from 'config/firebaseCourageous';
+import { Loading } from 'components/Loading'
+
+import { getUUID } from '../utils/utils';
+
+export const Producto = () => {
+  const { id } = useParams();
+  console.log(id);
+
+  const [descripcion, setDescripcion] = useState('');
+  const [estado, setEstado] = useState('');
+  const [valor, setValor] = useState('');
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
+
+  //Hook Formulario
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    if (data) {
+      console.log(data);
+      if (id === 'create') {
+        handleGuardarProducto();
+      } else {
+        handleActualizarProducto();
+      }
+    }
+  }; // your form submit function which will invoke after successful validation
+  const consultarProducto = async (idProducto) => {
+    setLoading(true);
+    console.log('idProducto: ', idProducto);
+    //const productoTemp = await consultarDocumentoDatabase('productos', idProducto)
+    const productoTemp = await buscarDocumentoFiltrado('productos', idProducto);
+    console.log('producto consultado:', productoTemp);
+    setDescripcion(productoTemp.descripcion);
+    setEstado(productoTemp.estado);
+    setValor(productoTemp.valor);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (id !== 'create') {
+      consultarProducto(id);
+    }
+
+    setDescripcion('');
+    setEstado('');
+    setValor('');
+  }, [id]);
+
+  const handleActualizarProducto = async (e) => {
+    //e.preventDefault();
+
+    const producto = {
+      descripcion,
+      estado,
+      valor,
+    };
+    // console.log(producto);
+
+    //await actualizarDocumentoDatabase('productos-courageous', id, producto);
+    setLoading(true);
+    await actualizarDocumentoFiltrado('productos', id, producto);
+    alert('¡Producto actualizado con éxito!');
+    setLoading(false);
+    history.push('/productos');
+  };
+
+  const handleGuardarProducto = async (e) => {
+    //e.preventDefault();
+
+    const producto = {
+      id: getUUID(),
+      descripcion,
+      estado,
+      valor,
+    };
+    setLoading(true);
+    await guardarDatabase('productos', producto);
+    alert('¡Producto registrado con éxito!');
+    setLoading(false);
+    history.push('/productos');
+  };
+
+  return (
+    <div className="container">
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <h1>{id === 'create' ? 'Crear ' : 'Editar '}Producto</h1>
+          <hr />
+          <div className='mt-3'>
+            <div className='row'>
+              <div className='offset-md-3 col-md-6'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className='mb-3'>
+                    <label className='form-label'>Descripción</label>
+                    <input
+                      className='form-control'
+                      type='text'
+                      required
+                      placeholder='Descripción'
+                      value={descripcion}
+                      onChange={(event) => setDescripcion(event.target.value)}
+                    />
+                    {/* {errors.desc && <p>This field is required</p>} */}
+                  </div>
+                  <div className='mb-3'>
+                    <label className='form-label'>Estado</label>
+                    <select
+                      id='inputEstadoProducto'
+                      class='form-select'
+                      required
+                      onChange={(event) => setEstado(event.target.value)}
+                    >
+                      {estado === '' ? (
+                        <>
+                          <option
+                            diabled
+                            value=''
+                            selected
+                            className='text-muted'
+                          >
+                            Seleccione un estado
+                          </option>
+                          <option value='Disponible'>Disponible</option>
+                          <option value=' No disponible'>No disponible</option>
+                        </>
+                      ) : estado === 'Disponible' ? (
+                        <>
+                          <option diabled value='' /* class='text-muted' */>
+                            Seleccione un estado
+                          </option>
+                          <option value='Disponible' selected>
+                            Disponible
+                          </option>
+                          <option value=' No disponible'>No disponible</option>
+                        </>
+                      ) : (
+                        <>
+                          <option diabled value='' /* class='text-muted' */>
+                            Seleccione un estado
+                          </option>
+                          <option value='Disponible'>Disponible</option>
+                          <option value=' No disponible' selected>
+                            No disponible
+                          </option>
+                        </>
+                      )}
+                      ;
+                    </select>
+                  </div>
+                  <div className='mb-3'>
+                    <label className='form-label'>Precio Unitario</label>
+                    <input
+                      className='form-control'
+                      type='number'
+                      min='0'
+                      required
+                      placeholder='Precio Unitario'
+                      value={valor}
+                      onChange={(event) => setValor(event.target.value)}
+                    />
+                  </div>
+                  <input
+                    type='submit'
+                    className='btn btn-primary'
+                    value={
+                      id === 'create'
+                        ? 'Guardar producto'
+                        : 'Actualizar producto'
+                    }
+                  />
+                  {/* <button
+                    type="button"
+                    className='btn btn-primary'
+                    // onClick={
+                    //   id === 'create'
+                    //     ? handleGuardarProducto
+                    //     : handleActualizarProducto
+                    // }
+                  >
+                    {id === 'create' ? 'Guardar' : 'Actualizar'} Producto
+                  </button> */}
+                </form>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
--- venta-gasolina/src/views/productos.jsx
+++ venta-gasolina/src/views/productos.jsx
@@ -1,11 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { consultarDatabase } from 'config/firebaseCourageous'
+import { Loading } from 'components/Loading'
 
-const Productos = () => {
-    return (
-        <div>
-            <h1>Productos</h1>
-        </div>
-    )
+
+export const Productos = () => {
+
+  const [listaProductos, setListaProductos] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const cargarProductos = async () => {
+    setLoading(true)
+    const listaTemporal = await consultarDatabase('productos')
+    // console.log(listaTemporal);
+    setListaProductos(listaTemporal)
+    setLoading(false)
+  }
+  // cargarProductos()
+
+  useEffect(() => {
+    cargarProductos()
+  }, [])
+
+
+  return (
+    <div className="container px-5 pt-4">
+      {
+        loading
+          ?
+          <Loading />
+          :
+          <>
+            <h1>
+              Lista Productos
+              <Link to="/productos/create"
+                className="btn btn-outline-success float-end"
+              >Adicionar Producto</Link>
+            </h1>
+            <hr />
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Descripcion</th>
+                  <th scope="col">Estado</th>
+                  <th scope="col">Valor</th>
+                  <th scope="col">Accion</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  listaProductos.map((producto, index) => (
+                    <tr key={producto.id}>
+                      <th scope="row">{index + 1}</th>
+                      <td>{producto.descripcion}</td>
+                      <td>{producto.estado}</td>
+                      <td>{producto.valor}</td>
+                      <td>
+                        <Link className="btn btn-outline-primary btn-sm"
+                          to={`/productos/${producto.id}`}>
+                          Editar
+                        </Link>
+
+                      </td>
+                    </tr>
+                  ))
+                }
+
+              </tbody>
+            </table>
+          </>
+      }
+
+
+    </div>
+  )
 }
-
-export default Productos;
