1539e8ee9f46aa6c9117145538f57071bb8a91b3
Andres-B-Developer <faburbano@unicauca.edu.co>
2021-10-17 10:28:24 -0500

    MOD App.jsx: Cambio rutas

--- venta-gasolina/src/App.jsx
+++ venta-gasolina/src/App.jsx
@@ -2,44 +2,26 @@
 import Header from './components/Header';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Ventas from 'views/Ventas';
-import {Productos} from 'views/Productos';
-import {Producto} from 'components/Producto';
+import { Productos } from 'views/Productos';
+import { Producto } from 'components/Producto';
 import Usuarios from 'views/Usuarios';
 import Login from 'views/Login';
-
-
 
 function App() {
   return (
     <>
-
-
-    {/*  */}
-
+      {/*  */}
 
       <Router>
         <Header />
         <Switch>
-          <Route exact path='/ventas' component={Ventas}>
-  
+          <Route exact path='/ventas' component={Ventas} />
+          <Route exact path='/productos' component={Productos} />
+          <Route exact path='/productos/:id' component={Producto} />
+          <Route exact path='/usuarios' component={Usuarios} />
+          <Route exact path='/'>
+            <Login />
           </Route>
-          <Route exact path='/productos' component={Productos}>
-          </Route>
-          <Route exact path='/productos/:id'>
-            <Producto />
-          </Route>
-          <Route exact path='/usuarios'>
-            <Usuarios />
-          </Route>
-
-        </Switch>
-      </Router>
-
-      <Router>
-        <Switch>
-        <Route exact path='/'>
-          <Login />
-        </Route>
         </Switch>
       </Router>
     </>
