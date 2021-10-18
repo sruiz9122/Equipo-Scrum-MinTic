42c870108a99966ee32785361a872cdc3d8e0088
Eliana N <eliana.navarro@uptc.edu.co>
2021-10-18 00:49:20 -0500

    app login

--- venta-gasolina/src/App.jsx
+++ venta-gasolina/src/App.jsx
@@ -1,14 +1,81 @@
-// import './App.css';
-import Header from './components/Header';
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+// // import './App.css';
+// import Header from './components/Header';
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+// import React, { useState } from 'react';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { Ventas } from "views/Ventas";
+// import { Venta } from "components/Venta";
+// import { Productos } from 'views/Productos';
+// import { Producto } from 'components/Producto';
+// import Usuarios from 'views/Usuarios';
+// import Login from 'views/Login';
+
+// function App() {
+//   const [isUsersignedIn, setIsUserSignedIn] = useState(true);
+
+//   const auth = getAuth();
+//   onAuthStateChanged(auth, (user) => {
+//     if (user) {
+//       return setIsUserSignedIn(true);
+//     }
+
+//     setIsUserSignedIn(false);
+//   })
+//   if (isUsersignedIn === true) {
+//   return (
+//     <>
+//       {/*  */}
+
+//       <Router>
+//       <Switch>
+//           <Route exact path="/" component={Ventas} />
+//          </Switch>
+//         <Header />
+//         <Switch>
+//           <Route exact path='/ventas' component={Ventas} />
+//           <Route exact path='/ventas/:id' component={Venta} />
+//           <Route exact path='/productos' component={Productos} />
+//           <Route exact path='/productos/:id' component={Producto} />
+//           <Route exact path='/usuarios' component={Usuarios} />
+//           <Route exact path='/'>
+//             <Login />
+//           </Route>
+//         </Switch>
+//       </Router>
+//     </>
+//    );
+//   }
+//   else {
+//     return (
+//       <Router>
+//         <Switch>
+//           <Route path="/" component={Login} />
+//         </Switch>
+//       </Router>
+//     );
+//   }
+// }
+// export default App;
+import './App.css'
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Ventas } from "views/Ventas";
-import { Venta } from "components/Venta";
-import { Productos } from 'views/Productos';
-import { Producto } from 'components/Producto';
-import Usuarios from 'views/Usuarios';
+//import auth from '@react-firebase/auth'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+// import PublicLayout from 'layouts/PublicLayout';
+// import LayoutLogin from 'layouts/LayoutLogin';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+}
+  from "react-router-dom";
+// import Home from 'views/Home';
 import Login from 'views/Login';
+import {Ventas} from 'views/Ventas';
+import Productos from 'views/Productos';
+import Usuarios from 'views/Usuarios';
+
+//import firebase from 'components/firebase';
 
 function App() {
   const [isUsersignedIn, setIsUserSignedIn] = useState(true);
@@ -22,28 +89,16 @@ function App() {
     setIsUserSignedIn(false);
   })
   if (isUsersignedIn === true) {
-  return (
-    <>
-      {/*  */}
-
-      <Router>
-      <Switch>
+    return (
+      <>
+<Router>
+     <Switch>
           <Route exact path="/" component={Ventas} />
          </Switch>
-        <Header />
-        <Switch>
-          <Route exact path='/ventas' component={Ventas} />
-          <Route exact path='/ventas/:id' component={Venta} />
-          <Route exact path='/productos' component={Productos} />
-          <Route exact path='/productos/:id' component={Producto} />
-          <Route exact path='/usuarios' component={Usuarios} />
-          <Route exact path='/'>
-            <Login />
-          </Route>
-        </Switch>
-      </Router>
-    </>
-   );
+       </Router>
+</>
+
+    );
   }
   else {
     return (
@@ -56,4 +111,5 @@ function App() {
   }
 }
 export default App;
+
 
