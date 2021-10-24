b2a7a8b1eb5f0e42b2b1d98843960019a074da38
Juan Andrés Urrego Moreno <j.a.u.m.06@hotmail.com>
2021-10-24 17:24:54 -0500

    MOD: se agrega validación de ventanas por rol

--- src/App.jsx
+++ src/App.jsx
@@ -20,40 +20,60 @@ import {
 }
   from "react-router-dom";
 import Login from 'views/Login';
-import {Ventas} from 'views/Ventas';
+import { Ventas } from 'views/Ventas';
 import Usuarios from 'views/Usuarios';
+import { buscarDocumentoFiltrado } from "config/firebaseCourageous";
 
 
 function App() {
   const [isUsersignedIn, setIsUserSignedIn] = useState(true);
+  const [rolUser, setRolUser] = useState('');
+  let tempUser = '';
+
+  const handleRol = async (usuario) => {
+    tempUser = await buscarDocumentoFiltrado('usuarios', usuario.uid)
+    setRolUser(tempUser.rol)
+  }
 
   const auth = getAuth();
   onAuthStateChanged(auth, (user) => {
     if (user) {
+      handleRol(user)
       return setIsUserSignedIn(true);
     }
 
     setIsUserSignedIn(false);
   })
+  console.log();
   if (isUsersignedIn === true) {
+    if (rolUser === 'Vendedor') {
+      return (
+        <>
+          <Router>
+            <Switch>
+              <Route exact path='/ventas' component={Ventas} />
+              <Route exact path='/ventas/:id' component={Venta} />
+            </Switch>
+          </Router>
+        </>
+      )
+    }
     return (
       <>
-<Router>
-     
+        <Router>
+
           {/* <Header /> */}
-         <Switch>
-           
-           <Route exact path='/ventas' component={Ventas} />
-           <Route exact path='/ventas/:id' component={Venta} />
-           <Route exact path='/productos' component={Productos} />
-           <Route exact path='/productos/:id' component={Producto} />
-           <Route exact path="/Usuarios/:id" component={UsuarioFiltrado} />
-           <Route exact path='/Usuarios' component={Usuarios}/>
-           
-         </Switch>
-         {/* <Footer/> */}
-       </Router>
-</>
+          <Switch>
+            <Route exact path='/ventas' component={Ventas} />
+            <Route exact path='/ventas/:id' component={Venta} />
+            <Route exact path='/productos' component={Productos} />
+            <Route exact path='/productos/:id' component={Producto} />
+            <Route exact path="/Usuarios/:id" component={UsuarioFiltrado} />
+            <Route exact path='/Usuarios' component={Usuarios} />
+          </Switch>
+          {/* <Footer/> */}
+        </Router>
+      </>
 
     );
   }
